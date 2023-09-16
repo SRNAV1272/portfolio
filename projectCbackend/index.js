5091fbccb2fb4ee60622d259fbe141cdf43f5605
Sai Rajesh Korla <sairajesh.korla@navajna.com>
2023-09-16 13:51:13 +0530

    16-09-2023_changes

--- projectCbackend/index.js
+++ projectCbackend/index.js
@@ -19,7 +19,7 @@ let collection
 
 const app = express()
 
-const port = 5000
+const port = 8080
 app.use(express.json())
 app.use(cors())
 app.use(express.static(path.join(`${process.cwd()}`, "build")))
